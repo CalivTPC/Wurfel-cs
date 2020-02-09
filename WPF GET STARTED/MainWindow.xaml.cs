32cbcee33770b49d99a58f0744bc866e9a9030c1
skynear <skynearhd@gmail.com>
2020-02-09 16:37:16 +0100

    äs funktioniert aues abr äs isch hardcoded u nid sehr schön

--- WPF GET STARTED/MainWindow.xaml.cs
+++ WPF GET STARTED/MainWindow.xaml.cs
@@ -12,17 +12,102 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Timers;
 
 namespace WPF_GET_STARTED
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    
     public partial class MainWindow : Window
     {
+        
         public MainWindow()
         {
             InitializeComponent();
+            
+        }
+        private void bnt24_Click(object sender, RoutedEventArgs e)
+        {
+            Random rnd = new Random();
+            int number = rnd.Next(1, 7);
+            Console.WriteLine("number: " + number);
+            txb14.Text = Convert.ToString(number);
+            //effect()
+            if (number == 1)
+            {
+                btn11.Visibility = Visibility.Hidden;
+                btn12.Visibility = Visibility.Hidden;
+                btn13.Visibility = Visibility.Hidden;
+                btn21.Visibility = Visibility.Hidden;
+                btn22.Visibility = Visibility.Visible;
+                btn23.Visibility = Visibility.Hidden;
+                btn31.Visibility = Visibility.Hidden;
+                btn32.Visibility = Visibility.Hidden;
+                btn33.Visibility = Visibility.Hidden;
+            };
+            if (number == 2)
+            {
+                btn11.Visibility = Visibility.Hidden;
+                btn12.Visibility = Visibility.Hidden;
+                btn13.Visibility = Visibility.Visible;
+                btn21.Visibility = Visibility.Hidden;
+                btn22.Visibility = Visibility.Hidden;
+                btn23.Visibility = Visibility.Hidden;
+                btn31.Visibility = Visibility.Visible;
+                btn32.Visibility = Visibility.Hidden;
+                btn33.Visibility = Visibility.Hidden;
+            };
+            if (number == 3)
+            {
+                btn11.Visibility = Visibility.Hidden;
+                btn12.Visibility = Visibility.Hidden;
+                btn13.Visibility = Visibility.Visible;
+                btn21.Visibility = Visibility.Hidden;
+                btn22.Visibility = Visibility.Visible;
+                btn23.Visibility = Visibility.Hidden;
+                btn31.Visibility = Visibility.Visible;
+                btn32.Visibility = Visibility.Hidden;
+                btn33.Visibility = Visibility.Hidden;
+            };
+            if (number == 4)
+            {
+                btn11.Visibility = Visibility.Visible;
+                btn12.Visibility = Visibility.Hidden;
+                btn13.Visibility = Visibility.Visible;
+                btn21.Visibility = Visibility.Hidden;
+                btn22.Visibility = Visibility.Hidden;
+                btn23.Visibility = Visibility.Hidden;
+                btn31.Visibility = Visibility.Visible;
+                btn32.Visibility = Visibility.Hidden;
+                btn33.Visibility = Visibility.Visible;
+            };
+            if (number == 5)
+            {
+                btn11.Visibility = Visibility.Visible;
+                btn12.Visibility = Visibility.Hidden;
+                btn13.Visibility = Visibility.Visible;
+                btn21.Visibility = Visibility.Hidden;
+                btn22.Visibility = Visibility.Visible;
+                btn23.Visibility = Visibility.Hidden;
+                btn31.Visibility = Visibility.Visible;
+                btn32.Visibility = Visibility.Hidden;
+                btn33.Visibility = Visibility.Visible;
+            }
+            if (number == 6)
+            {
+                btn11.Visibility = Visibility.Visible;
+                btn12.Visibility = Visibility.Hidden;
+                btn13.Visibility = Visibility.Visible;
+                btn21.Visibility = Visibility.Visible;
+                btn22.Visibility = Visibility.Hidden;
+                btn23.Visibility = Visibility.Visible;
+                btn31.Visibility = Visibility.Visible;
+                btn32.Visibility = Visibility.Hidden;
+                btn33.Visibility = Visibility.Visible;
+            };
+        }
         }
     }
 }
